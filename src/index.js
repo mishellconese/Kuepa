62eae99aa4bd8438460b05ddbb379e510042d2fa
andrea <andreaforero502@gmail.com>
2021-08-05 15:06:28 -0500

    corregí ubicacion de index.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../../../index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './firebase'
 import 'bootswatch/dist/superhero/bootstrap.min.css'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
-s
